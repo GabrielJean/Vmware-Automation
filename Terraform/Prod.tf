0c1505a1b486bc534cd45ac9be31ae8702bc823e
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-06-13 21:36:54 -0400

    added exemple module

--- Terraform/Prod.tf
+++ Terraform/Prod.tf
@@ -1,5 +1,6 @@
-module "Automation" {
+module "Exemple" {
   source = "./modules/Ubuntu 20.04/"
-  vsphere_virtual_machine_name = "Automation"
+  vsphere_virtual_machine_name = "Exemple"
   vsphere_virtual_machine_ip = "10.0.0.25"
+  vsphere_virtual_machine_ip = "ehq01-DT"
 }

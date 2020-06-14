8965878dff5c68b8dbf5321b2ed65a1e5be84b0b
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-06-14 16:36:16 -0400

    added Nginx vm

--- Terraform/Prod.tf
+++ Terraform/Prod.tf
@@ -1,6 +1,13 @@
-module "Exemple" {
+# module "Exemple" {
+#   source = "./modules/Ubuntu 20.04/"
+#   vsphere_virtual_machine_name = "Exemple"
+#   vsphere_virtual_machine_ip = "10.0.0.25"
+#   vsphere_virtual_machine_ip = "ehq01-DT"
+# }
+
+module "Nginx" {
   source = "./modules/Ubuntu 20.04/"
-  vsphere_virtual_machine_name = "Exemple"
-  vsphere_virtual_machine_ip = "10.0.0.25"
-  vsphere_virtual_machine_ip = "ehq01-DT"
+  vsphere_virtual_machine_name = "Nginx"
+  vsphere_virtual_machine_ip = "10.0.0.30"
+  vsphere_virtual_machine_ip = "FreeNAS-DT"
 }

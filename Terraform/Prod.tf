2a1e992e4e34cdcb7e670d9cfecc75cb63899a4e
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-06-14 16:37:42 -0400

    fixed datastore variable

--- Terraform/Prod.tf
+++ Terraform/Prod.tf
@@ -2,12 +2,12 @@
 #   source = "./modules/Ubuntu 20.04/"
 #   vsphere_virtual_machine_name = "Exemple"
 #   vsphere_virtual_machine_ip = "10.0.0.25"
-#   vsphere_virtual_machine_ip = "ehq01-DT"
+#   vsphere_virtual_machine_Datastore = "ehq01-DT"
 # }
 
 module "Nginx" {
   source = "./modules/Ubuntu 20.04/"
   vsphere_virtual_machine_name = "Nginx"
   vsphere_virtual_machine_ip = "10.0.0.30"
-  vsphere_virtual_machine_ip = "FreeNAS-DT"
+  vsphere_virtual_machine_Datastore = "FreeNAS-DT"
 }

9718fc8b92ad036016969568b076aec3919bd123
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-07-03 22:49:18 -0400

    added azure storage backend for state on terraform

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -6,3 +6,14 @@ provider "vsphere" {
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "gj-storage-rg"
+    storage_account_name = "gjstoragehot"
+    container_name       = "tfstate"
+    key                  = ""
+  }
+}
+

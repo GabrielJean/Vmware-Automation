1563d69eaa4e88d6810408eef84e8b5b3c40d820
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-08-16 19:38:41 -0400

    added default variables to Terraform

--- Terraform/modules/Ubuntu 20.04/variables.tf
+++ Terraform/modules/Ubuntu 20.04/variables.tf
@@ -11,12 +11,14 @@ variable "vsphere_virtual_machine_ip" {
 variable "vsphere_virtual_machine_gw" {
   description = "Define Virtual Machine Gateway"
   type        = string
+  default     = "10.0.0.1"
 }
 
 
 variable "vsphere_virtual_machine_network" {
   description = "Define Virtual Machine Network"
   type        = string
+  default     = "VM Network"
 }
 
 variable "vsphere_virtual_machine_Datastore" {
@@ -27,14 +29,17 @@ variable "vsphere_virtual_machine_Datastore" {
 variable "vsphere_virtual_machine_tag" {
   description = "Define ansible group"
   type        = string
+  default     = "base"
 }
 
 variable "vsphere_virtual_machine_cpu" {
   description = "Define amount of virtual cpu"
   type        = number
+  default     = 1
 }
 
 variable "vsphere_virtual_machine_ram" {
   description = "Define amount of ram"
   type        = number
+  default     = 1024
 }

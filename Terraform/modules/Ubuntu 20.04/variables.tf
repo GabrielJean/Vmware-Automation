e8e9a47a50e0a98c46a2a2e702566979dbdfc769
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-08-22 19:28:13 -0400

    updated tag names

--- Terraform/modules/Ubuntu 20.04/variables.tf
+++ Terraform/modules/Ubuntu 20.04/variables.tf
@@ -29,7 +29,7 @@ variable "vsphere_virtual_machine_Datastore" {
 variable "vsphere_virtual_machine_tag" {
   description = "Define ansible group"
   type        = string
-  default     = "base"
+  default     = "Base"
 }
 
 variable "vsphere_virtual_machine_cpu" {

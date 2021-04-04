1ed0340a74f17c4fc0ee442cb0a50d24050e3f59
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2021-04-04 16:00:24 -0400

    scaled k3s cluster

--- Terraform/K3s.tf
+++ Terraform/K3s.tf
@@ -26,22 +26,22 @@ module "K3s-Master-2" {
   ]
 }
 
-# module "K3s-Master-3" {
-#   source = "./modules/Ubuntu 20.04/"
-#   vsphere_virtual_machine_name = "K3s-Master-3"
-#   vsphere_virtual_machine_network = "APP"
-#   vsphere_virtual_machine_ip = "10.0.30.53"
-#   vsphere_virtual_machine_gw = "10.0.30.1"
-#   vsphere_virtual_machine_Datastore = "ehq03-DS"
-#   vsphere_virtual_machine_tag = "K3s-Master-join"
-#   vsphere_virtual_machine_ram = 1024
-#   vsphere_virtual_machine_cpu = 2
+module "K3s-Master-3" {
+  source = "./modules/Ubuntu 20.04/"
+  vsphere_virtual_machine_name = "K3s-Master-3"
+  vsphere_virtual_machine_network = "APP"
+  vsphere_virtual_machine_ip = "10.0.30.53"
+  vsphere_virtual_machine_gw = "10.0.30.1"
+  vsphere_virtual_machine_Datastore = "ehq03-DS"
+  vsphere_virtual_machine_tag = "K3s-Master-join"
+  vsphere_virtual_machine_ram = 1024
+  vsphere_virtual_machine_cpu = 2
 
-  # depends_on = [
-  #   module.K3s-Master-1
-  # ]
+  depends_on = [
+    module.K3s-Master-1
+  ]
 
-# }
+}
 
 
 module "K3s-Worker-1" {
@@ -60,18 +60,18 @@ module "K3s-Worker-1" {
   ]
 }
 
-# module "K3s-Worker-2" {
-#   source = "./modules/Ubuntu 20.04/"
-#   vsphere_virtual_machine_name = "K3s-Worker-2"
-#   vsphere_virtual_machine_network = "APP"
-#   vsphere_virtual_machine_ip = "10.0.30.56"
-#   vsphere_virtual_machine_gw = "10.0.30.1"
-#   vsphere_virtual_machine_Datastore = "ehq02-DS"
-#   vsphere_virtual_machine_tag = "K3s-Worker-join"
-#   vsphere_virtual_machine_ram = 8192
-#   vsphere_virtual_machine_cpu = 3
+module "K3s-Worker-2" {
+  source = "./modules/Ubuntu 20.04/"
+  vsphere_virtual_machine_name = "K3s-Worker-2"
+  vsphere_virtual_machine_network = "APP"
+  vsphere_virtual_machine_ip = "10.0.30.56"
+  vsphere_virtual_machine_gw = "10.0.30.1"
+  vsphere_virtual_machine_Datastore = "ehq02-DS"
+  vsphere_virtual_machine_tag = "K3s-Worker-join"
+  vsphere_virtual_machine_ram = 8192
+  vsphere_virtual_machine_cpu = 3
 
-#   depends_on = [
-#     module.K3s-Master-1
-#   ]
-# }
+  depends_on = [
+    module.K3s-Master-1
+  ]
+}
